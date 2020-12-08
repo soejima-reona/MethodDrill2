56eb6dea08f80d44f45a5489a964df96cccc65b3
*zd8LA4IHbi <soejima.reona@m.alhinc.jp>
2020-12-08 14:41:47 +0900

    first commit

---
+++ src/method_drill/Question01.java
@@ -0,0 +1,15 @@
+package method_drill;
+
+public class Question01 {
+
+	static void printMessage(String message) {
+
+		System.out.println(message);
+	}
+
+	public static void main(String[] args) {
+
+		// Question01 のメソッドの動作検証
+		printMessage("Hello");
+	}
+}
