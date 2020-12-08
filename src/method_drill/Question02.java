9ca19ef4adbf40571d17c7ce58b1d5444a08fbf0
*zd8LA4IHbi <soejima.reona@m.alhinc.jp>
2020-12-08 15:35:52 +0900

    Question02

--- src/method_drill/Question02.java
+++ src/method_drill/Question02.java
@@ -1,13 +1,16 @@
 package method_drill;
 
-//2
-
 public class Question02 {
 
 	static double printCircleArea(double radius) {
 
-		return radius * radius * PI;
-
+		//円の面積＝半径(引数)*半径(引数)*円周率
+		return radius * radius * Math.PI;
 	}
 
+	public static void main(String[] args) {
+		// Question02のメソッドの動作検証
+		double prinCircleArea = printCircleArea(2.0);
+		System.out.println("円の面積は" + prinCircleArea);
+	}
 }
