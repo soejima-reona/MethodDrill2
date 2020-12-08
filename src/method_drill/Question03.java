c8f62f969959102f841db6f6de2e0b6d2fa5d1c2
*zd8LA4IHbi <soejima.reona@m.alhinc.jp>
2020-12-08 16:22:58 +0900

    Question03

---
+++ src/method_drill/Question03.java
@@ -0,0 +1,20 @@
+package method_drill;
+
+public class Question03 {
+
+	static void printMessage(String message, int count) {
+
+		//変数iが、引数countより小さいときはメッセージ出力（countより大きくなったら抜ける）
+		for (int i = 0; i < count; i++) {
+			System.out.println(message);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// Question03 のメソッドの動作検証
+		printMessage("Hello", 5);
+
+	}
+
+}
