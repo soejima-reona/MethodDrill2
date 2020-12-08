207f385ebde6fcc4dbdecb10015c8c2db7a7d674
*zd8LA4IHbi <soejima.reona@m.alhinc.jp>
2020-12-08 16:45:21 +0900

    Question05

---
+++ src/method_drill/Question05.java
@@ -0,0 +1,20 @@
+package method_drill;
+
+public class Question05 {
+
+	static boolean isEvenNumber(int value) {
+
+		//引数が偶数＝true、　奇数＝false　を返す
+		if (value % 2 == 0) {
+			return true;
+		} else
+			return false;
+	}
+
+	public static void main(String[] args) {
+
+		boolean number = isEvenNumber(2000);
+		System.out.println(number);
+	}
+
+}
